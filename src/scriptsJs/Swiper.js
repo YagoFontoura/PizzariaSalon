3b6982c7a75c4c3afb5e80b39ff781094ea34b0b
Yago B. Fontoura <yagofontoura222@yahoo.com>
2021-09-23 01:34:59 -0300

    bugfix and MediaQuery

--- src/scriptsJs/Swiper.js
+++ src/scriptsJs/Swiper.js
@@ -5,4 +5,14 @@ const  swiper = new Swiper('.swiper', {
     },
     mouseWheel:true,
     keyboard: true,
+    breakpoints: {
+      767: {
+        slidesPerView: 2,
+        setWrapperSize: true
+      },
+      1300: {
+        slidesPerView: 3,
+        setWrapperSize:true
+      }
+    }
   });
--- src/scriptsJs/main.js
+++ src/scriptsJs/main.js
@@ -42,7 +42,28 @@ function backToTop() {
     }
 }
 
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection(){
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
 
+    for(const section of sections) {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+        const sectionId = section.getAttribute('id');
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if(checkpointStart && checkpointEnd) {
+            document.querySelector('nav ul li a[href*=' + sectionId + ']')
+            .classList.add('active')
+        
+        } else {
+            document.querySelector('nav ul li a[href*=' + sectionId + ']')
+            .classList.remove('active')
+        }
+    }
+}
 
 
 
@@ -51,6 +72,7 @@ function backToTop() {
 window.addEventListener('scroll', function(){
     changeHeaderWhenScroll()
     backToTop()
+    activateMenuAtCurrentSection()
 });
 
 
