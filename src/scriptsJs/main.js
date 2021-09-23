f7353ca7f25b080a6a5c9281cb9d476ed1db1779
Yago B. Fontoura <yagofontoura222@yahoo.com>
2021-09-23 01:40:51 -0300

    bugfix and mediaQuery

--- src/scriptsJs/main.js
+++ src/scriptsJs/main.js
@@ -42,28 +42,7 @@ function backToTop() {
     }
 }
 
-const sections = document.querySelectorAll('main section[id]')
-function activateMenuAtCurrentSection(){
-    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
 
-    for(const section of sections) {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.offsetHeight;
-        const sectionId = section.getAttribute('id');
-
-        const checkpointStart = checkpoint >= sectionTop
-        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
-
-        if(checkpointStart && checkpointEnd) {
-            document.querySelector('nav ul li a[href*=' + sectionId + ']')
-            .classList.add('active')
-        
-        } else {
-            document.querySelector('nav ul li a[href*=' + sectionId + ']')
-            .classList.remove('active')
-        }
-    }
-}
 
 
 
@@ -72,7 +51,7 @@ function activateMenuAtCurrentSection(){
 window.addEventListener('scroll', function(){
     changeHeaderWhenScroll()
     backToTop()
-    activateMenuAtCurrentSection()
+    
 });
 
 
